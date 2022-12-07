baec9729088575aa05c866ee79123ecc12ba7c31
Felipe Frazão <felipe.frazao@gmail.com>
2022-12-07 20:10:10 -0300

    A disponibilizar a nossa aplicação no servlet container Jetty Que Servlet é uma especificação Que a especificação Servlet faz parte do Java EE/Jakarta EE Que, ao usar Servlet, programamos independentemente do servidor/container A diferença entre servlet container e application server

--- src/main/java/br/com/felipe/gerenciador/servlet/AutorizacaoFilter.java
+++ src/main/java/br/com/felipe/gerenciador/servlet/AutorizacaoFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -16,6 +17,12 @@ import javax.servlet.http.HttpSession;
 //@WebFilter("/entrada") //funcionando pelo xml
 public class AutorizacaoFilter implements Filter {
 
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {}
+
+	@Override
+	public void destroy() {}
+	
 	@Override
 	public void doFilter(ServletRequest requestServlet, ServletResponse responseServlet, FilterChain chain)
 			throws IOException, ServletException {
